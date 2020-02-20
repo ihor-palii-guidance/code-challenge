4748b4cccc8b7f129fe412941277af55f92289b4
Ihor Palii <ihor.palii@guidance.com>
2020-02-20 19:02:31 +0200

    Solved tasks

--- javascript/hamming/index.js
+++ javascript/hamming/index.js
@@ -3,5 +3,24 @@
 // your code meets all of the conditions.
 // You may use ES6 or ES5 to solve.
 
+const zip = (arr, ...arrs) => {
+    return arr.map((val, i) => arrs.reduce((a, arr) => [...a, arr[i]], [val]));
+}
+
+class Hamming {
+    compute(a, b) {
+        a = Array.from(a)
+        b = Array.from(b)
+        if (a.length !== b.length) {
+            throw "DNA strands must be of equal length.";
+        } else {
+            var i = 0;
+            zip(a, b).forEach(pair => {
+                if (pair[0] !== pair[1]) i++;
+            });
+            return i;
+        }
+    }
+}
 
 module.exports = Hamming;
--- python/hamming/__init__.py
+++ python/hamming/__init__.py
@@ -3,5 +3,7 @@
 # unit tests pass and your code meets all of the conditions.
 #
 
-def compute():
-    pass
+def compute(a, b):
+    if len(a) == len(b): 
+        return sum(1 for x, y in zip(a, b) if x != y)
+    raise ValueError
